81ab44dbf55687af76d55ceefc7d3001db534f57
kripanidhid-beep <kripanidhid@gmail.com>
2026-01-16 13:54:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,5 @@ def run_flask():
 if __name__ == "__main__":
     threading.Thread(target=run_flask).start()
     print("Bot is starting...")
-    # non_stop=True aur interval=0 hata kar simple polling rakhein
-    bot.polling(none_stop=True, timeout=60)
+    bot.infinity_polling(timeout=10, long_polling_timeout=5)
 
