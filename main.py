9adc0d1851f452696ba5e29c59d8061b15227bbe
kripanidhid-beep <kripanidhid@gmail.com>
2026-01-16 11:07:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ def home():
     return "Pandey Je Bot is LIVE and Rocking! 🔥"
 
 def run_flask():
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000)) 
     app.run(host='0.0.0.0', port=port)
+
 
 # --- Bot Configurations ---
 TOKEN = os.getenv("TOKEN")
